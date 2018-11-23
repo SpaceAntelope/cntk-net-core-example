5e37968cb9b360480771555e123771129bd74fcb
AT <cernunos_kav@msn.com>
2018-11-23 15:47:13 +0200

    Added automatic dependency path resolution

--- cs-cntk-core/Program.cs
+++ cs-cntk-core/Program.cs
@@ -1,11 +1,53 @@
 ﻿using System;
 using System.Linq;
 using CNTK;
+using System.Reflection;
+using System.IO;
+
 
 namespace cs_cntk_core
 {
-    class Program
+    public static class Extensions
     {
+        public static TOut Pipe<TIn, TOut>(this TIn obj, Func<TIn, TOut> f) => f(obj);
+        public static void Pipe<TIn>(this TIn obj, Action<TIn> f) => f(obj);
+    }
+
+    public class Program
+    {
+        static void ResolveDepPaths()
+        {
+
+            var cntkPath = Assembly.GetAssembly(typeof(CNTK.CNTKLib)).CodeBase.Replace("file:///", "");
+            var supportPath = Path.GetFullPath(Path.Combine(cntkPath, "../../../Support/x64/Release"));
+
+            Console.WriteLine(supportPath, cntkPath);
+            var baseIndex = cntkPath.Split('/').ToList().FindIndex(dir => dir.ToLower().Contains("cntk."));
+
+            Console.WriteLine(baseIndex.ToString());
+
+            var cudaPaths = cntkPath
+                .Split("/")
+                .Take(baseIndex)
+                .Aggregate((state, current) => $"{state}/{current}")
+                .Pipe(path => Directory
+                        .GetDirectories(path, "cntk.deps.cu*")
+                        .SelectMany(dir => Directory.GetDirectories(Path.Combine(dir, "2.6.0/support/x64"))
+                        .Select(Path.GetFullPath)));
+
+            Console.WriteLine(supportPath);
+
+            var paths = Environment.GetEnvironmentVariable("PATH").Split(';').ToList();
+            paths.Add(Path.GetDirectoryName(cntkPath));
+            paths.Add(supportPath);
+            paths.AddRange(cudaPaths);
+
+            Environment.SetEnvironmentVariable("PATH", string.Join(';', paths));
+
+            Console.WriteLine("Paths");
+            Environment.GetEnvironmentVariable("PATH").Split(";").ToList().ForEach(Console.WriteLine);
+        }
+
         static void Main(string[] args)
         {
             //var pathVariable = Environment.GetEnvironmentVariable("PATH");
@@ -17,7 +59,7 @@ namespace cs_cntk_core
             //            .CodeBase)
             //        .Replace("file:\\","");
             //Environment.SetEnvironmentVariable("PATH", $"{pathVariable};{binPath}", EnvironmentVariableTarget.Process);
-  
+
             //Environment.GetEnvironmentVariable("PATH").Split(';').ToList().ForEach(Console.WriteLine);
 
             Console.WriteLine("Hello World!");
